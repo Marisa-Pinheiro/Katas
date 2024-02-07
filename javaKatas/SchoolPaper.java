07991925af9d81d495e2e3122e5fc935cfe9473e
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-07 10:19:45 +0100

    feat: schoolpaperwork kata from codewars

---
+++ javaKatas/SchoolPaper.java
@@ -0,0 +1,10 @@
+package javaKatas;
+
+public class SchoolPaper {
+        public static int paperWork(int n, int m) {
+            if (n < 0 || m < 0) {
+                return 0;
+            }
+            return n * m;
+        }
+}
